50699ae194fddfde02b266bc779f77f2b12edc88
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-01-21 08:03:42 +0000

    card ui changes

--- web/hc_web_manager/src/components/device_list/cards/rgb_lights_card.tsx
+++ web/hc_web_manager/src/components/device_list/cards/rgb_lights_card.tsx
@@ -8,6 +8,55 @@ import { Power } from "lucide-react";
 import { motion } from "motion/react";
 import { useEffect } from "react";
 
+
+function ColorButton({ enabled, r, g, b, setColor }: { enabled: boolean | null, r: number, g: number, b: number, setColor: (r: number, g: number, b: number) => void }) {
+    return (
+        <>
+            {enabled ? (
+                <motion.button
+                    initial={{ scale: 0 }}
+                    animate={{ scale: 1 }}
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.96 }}
+                    onClick={() => setColor(r, g, b)}
+                >
+                    <div
+                        style={{
+                            backgroundColor: `rgb(${r}, ${g}, ${b})`
+                        }}
+                        className="p-2 rounded"
+                    />
+                </motion.button>
+            ) : (
+                <div
+                    className="p-2 rounded bg-bg-light"
+                />
+            )}
+        </>
+
+    )
+};
+
+function ProgramButton({ program, startProgram }: { program: RGBLightsProgramT, startProgram: (program: RGBLightsProgramT) => void }) {
+    return (
+        <motion.button
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.96 }}
+            onClick={() => startProgram(program)}
+        >
+            <div
+                style={{
+                    //backgroundColor: `rgb(${r}, ${g}, ${b})`
+                }}
+                className="p-2 rounded"
+            />
+        </motion.button>
+    )
+}
+
+
 export function RGBLightsCard({ device }: { device: DeviceT }) {
     const { loading, state, ctx } = useDeviceState(device.deviceId, stateDecode);
 
@@ -23,33 +72,9 @@ export function RGBLightsCard({ device }: { device: DeviceT }) {
         ctx?.sendCommand(device.deviceId, Commands.startProgram(program));
     };
 
-    const stopProgram = () => {
-        ctx?.sendCommand(device.deviceId, Commands.stopProgram());
-    };
-
     useEffect(() => {
         console.log(state);
     }, [state]);
-
-    if (loading) {
-        return (
-            <div className="bg-bg-dark rounded w-64">
-                <div className="h-20 p-3 grid grid-cols-[1fr_3fr] items-center">
-                    <div>
-                        <div className="bg-bg-light rounded-full p-3 w-fit h-fit">
-                            <LoadingSpinnerImage width={30} height={30} />
-                        </div>
-                    </div>
-                    <div className="grow flex justify-center">
-                        <h1 className="text-fg-light">{device.name}</h1>
-                    </div>
-                </div>
-                <div className="h-9 p-3 bg-bg-medium flex justify-center items-center">
-                    <p className="text-xs text-fg-medium"></p>
-                </div>
-            </div>
-        )
-    }
 
     if (!loading && !state) {
         return (
@@ -57,46 +82,59 @@ export function RGBLightsCard({ device }: { device: DeviceT }) {
         )
     }
 
-    if (!state) return;
+    const ready = state && !loading;
 
     return (
         <motion.div
             className="bg-bg-dark rounded w-64"
-            style={{ boxShadow: `0 0 10px 1px rgb(${state.r}, ${state.g}, ${state.b})` }}
+            style={{ boxShadow: ready ? (`0 0 30px 1px rgb(${state.r}, ${state.g}, ${state.b})`) : `` }}
         >
-            <div className="h-20 p-3">
-                <div className="grid grid-cols-[1fr_3fr] items-center">
+            <div className="h-20">
+                <div className="p-2 h-full grid grid-cols-[1fr_3fr] items-center">
                     <div className="flex justify-center">
-                        <motion.button
-                            className={`${state.powered ? 'bg-green-600' : 'bg-bg-light'} rounded-full p-3`}
-                            initial={{ scale: 0 }}
-                            animate={{ scale: 1 }}
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.96 }}
-                            onClick={togglePower}
-                        >
-                            <Power width={30} height={30} className="text-fg-dark" />
-                        </motion.button>
+                        {ready ? (
+                            <motion.button
+                                className={`${state.powered ? 'bg-bg-accent' : 'bg-bg-light'} rounded-full p-3`}
+                                initial={{ scale: 0 }}
+                                animate={{ scale: 1 }}
+                                whileHover={{ scale: 1.05 }}
+                                whileTap={{ scale: 0.96 }}
+                                onClick={togglePower}
+                            >
+                                <Power width={30} height={30} className={`${state.powered ? 'text-fg-accent' : 'text-fg-light'}`} />
+                            </motion.button>
+                        ) : (
+                            <div className="bg-bg-light rounded-full p-3 w-fit h-fit">
+                                <LoadingSpinnerImage width={30} height={30} />
+                            </div>
+                        )}
                     </div>
-                    <div className="flex grow justify-center">
-                        <h1 className="text-fg-dark">{device.name}</h1>
+                    <div className="h-full flex flex-col justify-center gap-2">
+                        <div className="flex justify-center">
+                            <h1 className={ready ? `text-fg-dark` : `text-fg-light`}>{device.name}</h1>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="flex gap-3 justify-center items-center">
+                                <ColorButton enabled={ready} r={255} g={0} b={0} setColor={setColor} />
+                                <ColorButton enabled={ready} r={0} g={255} b={0} setColor={setColor} />
+                                <ColorButton enabled={ready} r={255} g={0} b={255} setColor={setColor} />
+                                <ColorButton enabled={ready} r={0} g={255} b={255} setColor={setColor} />
+                                <ColorButton enabled={ready} r={255} g={255} b={255} setColor={setColor} />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="">
-                    <button onClick={() => setColor(255, 0, 0)}>Test1</button>
-                    <button onClick={() => setColor(0, 0, 255)}>Test2</button>
-                    <button onClick={() => startProgram('rainbowFade')}>Start</button>
-                    <button onClick={() => stopProgram()}>Stop</button>
                 </div>
             </div>
             <div className="h-9 p-3 bg-bg-medium flex justify-center items-center rounded-b">
-                <motion.p
-                    className="text-xs text-fg-medium"
-                    initial={{ scale: 0 }}
-                    animate={{ scale: 1 }}
-                >
-                    ONLINE, POWERED {state.powered ? 'ON' : 'OFF'}
-                </motion.p>
+                {state && !loading && (
+                    <motion.p
+                        className="text-xs text-fg-medium"
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                    >
+                        ONLINE, POWERED {state.powered ? 'ON' : 'OFF'}
+                    </motion.p>
+                )}
             </div>
         </motion.div>
     )
